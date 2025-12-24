57deb1ccf4a8b486993f769bfb6ddfacf680991f
ElkkuSorsa <eliaspeliaspoks@gmail.com>
2025-12-25 01:28:24 +0200

    v

--- questionnaire.service.worker.js
+++ questionnaire.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766618717|42304303035';
+const CACHE_VERSION = '1766618899|42486360436';
 /** @type {string} */
 const CACHE_PREFIX = 'questionnaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
